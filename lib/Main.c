133524efdfb1b60f5f980a1a581b2979125df467
dsheppard1 <dsheppard1@arizona.edu>
2026-04-28 13:18:12 -0700

    protyping shi

--- lib/Main.c
+++ lib/Main.c
@@ -1,9 +1,76 @@
-#define _CRT_SECURE_NO_WARNINGS_
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(void){
-    printf("fuck you momma");
-    
-    return 0;
+
+
+
+typedef struct card_s {
+
+	char suit[9]; // name of the suit (clubs, diamonds, hearts, spades)
+	int value; 
+	// any other attributes of your choice
+
+} card;
+
+typedef struct player_s {
+
+
+	char name[100];
+	int money; // how much money a player has
+	int bet;
+	int playerNum;
+	card hand[10];
+
+} player;
+
+typedef struct deck_s { // use a pointer for this in main? Dynamic memory alloc?
+
+	card cards[52]; //deck of cards
+	bool isTaken; //is true when a card is in play (either dealer or player has it);
+} deck;
+
+int handSum(player x) {
+	int sum = 0;
+
+
+	return sum;
+}
+
+
+
+
+
+void shuffle(deck x[]) { // shuffles the deck we are playing with
+
+
+
+}
+
+void initPlayer(player x) {
+
+}
+
+void printHand(player x) {
+
+}
+
+void resetHand(player x) { // resets player hand
+
+
+}
+
+
+int main(void) {
+
+	int numPlayers;
+
+
+
+
+	return 0;
+
+
 }
