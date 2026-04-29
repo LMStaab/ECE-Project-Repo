9b44e74cc36ff7b80f3252c1c3a8b6e41bbdc436
dsheppard1 <dsheppard1@arizona.edu>
2026-04-29 07:11:43 -0700

    Added player initialization

--- lib/Main.c
+++ lib/Main.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <time.h>
-#include <stdlib.h>
+
+
+
 
 typedef struct card_s {
 
@@ -16,11 +17,12 @@ typedef struct card_s {
 
 typedef struct player_s {
 
+
 	char name[100];
 	int money; // how much money a player has
 	int bet;
 	int playerNum;
-	card hand[14];
+	card hand[10];
 
 } player;
 
@@ -28,7 +30,6 @@ typedef struct deck_s { // use a pointer for this in main? Dynamic memory alloc?
 
 	card cards[52]; //deck of cards
 	bool isTaken; //is true when a card is in play (either dealer or player has it);
-    int currIdx;
 } deck;
 
 int handSum(player x) {
@@ -39,50 +40,21 @@ int handSum(player x) {
 }
 
 
-deck makeDeck(){
-    deck d;
-    char suits[4][9] = {"diamonds", "clubs", "hearts", "spades"};
-    int deckIdx = 0;
-    d.currIdx = 0;
-
-    for(int s = 0; s < 4; s++){
-        for(int i = 1; i <= 13; i++){ //1 = ace, 11, 12, 13 == jack, queen, king
-            card c;
-            strcpy(c.suit, suits[s]);
-            c.value = i;
-            d.cards[deckIdx] = c;
-        }
-    }
-    return d;
-}
 
 
-void shuffle(deck d) { // shuffles the deck we are playing with
-    for(int i = 0; i < 12345; i++){
-        srand(time(NULL));
-        int x = (rand() % (52));
-        int y = (rand() % (52));
-        card temp1 = d.cards[x];
-        card temp2 = d.cards[y];
-        d.cards[x] = temp2;
-        d.cards[y] = temp1;
-    }
-    d.currIdx = 0;
+
+void shuffle(deck x[]) { // shuffles the deck we are playing with
+
+
 
 }
 
-card drawTop(deck d){
-    // need to fix case for empty deck
-    if(d.currIdx == 52){
-        shuffleDeck(d);
-    }
-    card c = d.cards[d.currIdx];
-    d.currIdx++;
-    return  c;
-}
-
-void initPlayer(player x) {
-
+void initPlayer(player *x, int num) {
+	printf("Enter player %d Name: ", num);
+	scanf("%s", x->name);
+	x->money = 100;
+	x->bet = 0;
+	x->playerNum = num;
 }
 
 void printHand(player x) {
@@ -94,10 +66,19 @@ void resetHand(player x) { // resets player hand
 
 }
 
+
 int main(void) {
 
 	int numPlayers;
+	player crowd[4]; //array for players, max 4
+	int i;
 
+	printf("Enter the number of players: ");
+	scanf("%d", &numPlayers);
+
+	for (i =0; i < numPlayers; i++){
+		initPlayer(&crowd[i], i+1);
+	}
 
 
 
